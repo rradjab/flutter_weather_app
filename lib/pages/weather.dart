7fefb7a1e1bf50d0e240c66798c318d0ad23562c
rradjab <rradjab@yahoo.com>
2023-02-16 18:43:52 +0400

    ra

--- lib/pages/weather.dart
+++ lib/pages/weather.dart
@@ -14,33 +14,28 @@ class Weather extends StatefulWidget {
 class _MyWidgetState extends State<Weather> {
   late Future<CityWeather> _cityWeather;
 
-  String selectedCity = 'Baku';
   @override
   void initState() {
     super.initState();
-    _cityWeather = getWeather(selectedCity);
+    _cityWeather = getWeather('Baku');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        centerTitle: true,
-        title: Text(selectedCity),
         bottom: PreferredSize(
-          preferredSize: const Size.fromHeight(14.0),
-          child: Container(
-            color: Colors.lightBlue,
-            child: Autocomplete(
-              optionsBuilder: (TextEditingValue textEditingValue) {
-                return getCountries(textEditingValue.text);
-              },
-              onSelected: (String selection) {
-                setState(() {
-                  _cityWeather = getWeather(selection);
-                });
-              },
-            ),
+          preferredSize: const Size.fromHeight(0.0),
+          child: Autocomplete(
+            initialValue: const TextEditingValue(text: 'Baku'),
+            optionsBuilder: (TextEditingValue textEditingValue) {
+              return getCountries(textEditingValue.text);
+            },
+            onSelected: (String selection) {
+              setState(() {
+                _cityWeather = getWeather(selection);
+              });
+            },
           ),
         ),
       ),
@@ -53,8 +48,8 @@ class _MyWidgetState extends State<Weather> {
               cityWeather: cw,
             );
           } else if (snapshot.hasError) {
-            return Center(
-              child: Text("Səhv: ${snapshot.error.toString()}"),
+            return const Center(
+              child: Text("Yazılışda səhv"),
             );
           }
           return const Center(child: CircularProgressIndicator());
